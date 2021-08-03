51d26be5034a69afbf328f2a61a13c1273757604
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-03 13:01:24 +0100

    display can show credit

--- __test__/display.test.js
+++ __test__/display.test.js
@@ -13,8 +13,16 @@ describe('Display', () => {
 
   test('it can display the date', () => {
     const mockHistory = {
-      date: new Date('January 14, 2012, 11:00:00'),
+      date: new Date('January 10, 2012, 11:00:00'),
     };
-    expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n14/01/2012');
+    expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n10/01/2012 ||');
+  });
+
+  test('it can display the credit', () => {
+    const mockHistory = {
+      date: new Date('January 10, 2012, 11:00:00'),
+      credit: 1000,
+    };
+    expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n10/01/2012 || 1000.00 || ||');
   });
 });
--- src/display.js
+++ src/display.js
@@ -2,13 +2,20 @@ class Display {
   constructor() {
     this.HEADER = 'date || credit || debit || balance';
   }
+
   print(history) {
     if (!history) {
       return this.HEADER;
-    } 
-    let displayString;
-    let formattedDate = history.date.toLocaleDateString();
-    displayString = `${this.HEADER}\n${formattedDate}`;
+    }
+    let displayString = `${this.HEADER}\n`;
+    // add date to display string
+    const formattedDate = history.date.toLocaleDateString();
+    displayString += `${formattedDate} ||`;
+    // add credit to display string
+    if (history.credit) {
+      const formattedCredit = history.credit.toFixed(2);
+      displayString += ` ${formattedCredit} || ||`;
+    }
     return displayString;
   }
 }
