34f7be525d4e2a43cbe78149bcb69356c7eda39c
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-03 14:54:32 +0100

    refactored display tests

--- __test__/display.test.js
+++ __test__/display.test.js
@@ -2,6 +2,25 @@ const Display = require('../src/display');
 
 describe('Display', () => {
   let display;
+  const singleTransactionMock = [{
+    date: new Date('January 10, 2012, 11:00:00'),
+    credit: 0,
+    debit: 0,
+    balance: 900,
+  }];
+  const multipleTransactionMock = [
+    {
+      date: new Date('January 10, 2012, 11:00:00'),
+      credit: 1000,
+      debit: 0,
+      balance: 1000,
+    },
+    {
+      date: new Date('January 14, 2012, 11:00:00'),
+      credit: 0,
+      debit: 500,
+      balance: 500,
+    }];
 
   beforeEach(() => {
     display = new Display();
@@ -13,49 +32,22 @@ describe('Display', () => {
 
   describe('for one transaction', () => {
     test('it can display credit', () => {
-      const mockHistory = [{
-        date: new Date('January 10, 2012, 11:00:00'),
-        credit: 1000,
-        balance: 900,
-      }];
-      expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n10/01/2012 || 1000.00 || || 900.00');
+      singleTransactionMock[0].credit = 1000;
+      expect(display.print(singleTransactionMock)).toEqual('date || credit || debit || balance\n10/01/2012 || 1000.00 || || 900.00');
     });
     test('it can display debit', () => {
-      const mockHistory = [{
-        date: new Date('January 10, 2012, 11:00:00'),
-        credit: 0,
-        debit: 250,
-        balance: 900,
-      }];
-      expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n10/01/2012 || || 250.00 || 900.00');
+      singleTransactionMock[0].credit = 0;
+      singleTransactionMock[0].debit = 250;
+      expect(display.print(singleTransactionMock)).toEqual('date || credit || debit || balance\n10/01/2012 || || 250.00 || 900.00');
     });
     test('it can display balance', () => {
-      const mockHistory = [{
-        date: new Date('January 10, 2012, 11:00:00'),
-        credit: 0,
-        debit: 250,
-        balance: 900,
-      }];
-      expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n10/01/2012 || || 250.00 || 900.00');
+      expect(display.print(singleTransactionMock)).toEqual('date || credit || debit || balance\n10/01/2012 || || 250.00 || 900.00');
     });
   });
 
   describe('for multiple transactions', () => {
     test('it shows the transactions in reverse chronological order', () => {
-      const mockHistory = [
-        {
-          date: new Date('January 10, 2012, 11:00:00'),
-          credit: 1000,
-          debit: 0,
-          balance: 1000,
-        },
-        {
-          date: new Date('January 14, 2012, 11:00:00'),
-          credit: 0,
-          debit: 500,
-          balance: 500,
-        }];
-      expect(display.print(mockHistory)).toEqual('date || credit || debit || balance\n14/01/2012 || || 500.00 || 500.00\n10/01/2012 || 1000.00 || || 1000.00');
+      expect(display.print(multipleTransactionMock)).toEqual('date || credit || debit || balance\n14/01/2012 || || 500.00 || 500.00\n10/01/2012 || 1000.00 || || 1000.00');
     });
   });
 });
