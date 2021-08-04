7eacbbfb0e5cc21bc30c6604ed98a1e31737767b
Arif Ebrahim <74261924+ArifEbrahim@users.noreply.github.com>
2021-08-04 18:17:11 +0100

    removed web folder and index.html file

--- web/transaction.js
+++
@@ -1,9 +0,0 @@
-class Transaction {
-  constructor(credit = 0, debit = 0, balance = 0) {
-    this.date = new Date();
-    this.credit = credit;
-    this.debit = debit;
-    this.balance = balance;
-  }
-}
-
